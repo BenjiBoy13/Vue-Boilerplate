d42532b073458186328a69b9251b7c5fa62cf62d
BenjiBoy13 <ben99.planetatierra@gmail.com>
2020-03-16 17:04:53 -0600

    Changes to welcome message and improving readme file

--- README.md
+++ README.md
@@ -1,23 +1,69 @@
 # Vue JS Boilerplate 
 
-Hi, this is a vue js boilerplate with webpack, 
-already configured for your needs.
+Start your new Vue JS project with this friendly and
+easy to use boilerplate!. 
 
 ### Install 
 
 Go ahead and make a new directory in which you would like
-to hold your application locally, go ahead and 
+to hold the application locally, go ahead and 
 clone this boilerplate with the following command.
 
 `git clone https://github.com/BenjiBoy13/Vue-Boilerplate.git`
 
-After the repo is cloned go into the directory
-and run the following commands.
+After the repo is cloned go into the directory you just
+created and run the following commands.
 
 `npm install`
 
 `npm run start`
 
-This should launch the application on a webpack local server
-and in there you will see all the things you need to know
-about the architecture of the boilerplate.
+This will hopefully launch the application on a 
+webpack local server open in port 3000, lets now 
+break down the boiler architecture so you can understand
+how to use it and get your hands dirty.
+
+### Components
+
+You can find the Vue components under the
+**./src/vue/** directory, In which you will hold
+all of your vue components, simple, right?; Moving on.
+
+### Routing
+
+The routing file is located under the **root**
+of the project, this is where you will find yourself editing
+and adding more routes, oh, almost forgot, you will find this
+file under the name of **router.js**.
+
+### Styles
+
+By default the css preprocessor  **sass** is
+configured, you wil find the main file under the
+**root** folder named as **app.scss**, the rest of 
+your scss files should be created under this
+**./src/scss/** directory.
+
+### Build it!
+
+By running the command **npm run build** your app 
+will compile into 3 files under the **dist**
+folder, you will find your now bundled files.
+Html, Css and javascript.
+
+By default your app is going to be build in 
+**Development** mode, you can switch this to 
+production mode by editing your webpack config file 
+like so:
+
+```javascript
+module.exports = {
+    mode: "production",
+    // ...
+}
+```
+
+### Of you go!.
+
+That's all you need to know, have fun, happy codding!. 
+Cheers 🍺🍺.
--- src/vue/components/Home.vue
+++ src/vue/components/Home.vue
@@ -3,75 +3,12 @@
         <div class="card heading">
             <div class="card-body">
                 <h2 class="card-title">
-                    Welcome stranger!.
+                    Welcome!.
                 </h2>
-                <p class="card-text">
-                    If you are watching this it means that you manage to get the server
-                    started with success, Im a boilerplate for Vue JS projects, Im configured
-                    right out the box but here are some things you should know about me before
-                    you get your hands dirty.
-                </p>
-            </div>
-        </div>
-        <div class="card">
-            <div class="card-body">
-                <h3 class="card-title"> Components </h3>
-                <p class="card-text">
-                    You can find the Vue components under the
-                    <strong> ./src/vue/ </strong> path
-                </p>
                 <p class="card-text">
                     You are now watching the <strong> Home.vue </strong>
                     component located in <strong> ./src/vue/components/Home.vue </strong>
-                    as the name suggests, this is your home page element.
-                </p>
-            </div>
-        </div>
-        <div class="card">
-            <div class="card-body">
-                <h3 class="card-title"> Routing </h3>
-                <p class="card-text">
-                    The routing file is located under the <strong> root </strong>
-                    of the project, this is where you will find yourself editing
-                    and adding more routes, oh, almost forgot, you will find this
-                    file under the name of <strong> router.js </strong>
-                </p>
-            </div>
-        </div>
-        <div class="card">
-            <div class="card-body">
-                <h3 class="card-title"> Styles </h3>
-                <p class="card-text">
-                    By default the css preprocessor  <strong> sass </strong>
-                    is configured, you wil find the main file under the
-                    <strong> root </strong> folder named as <strong> app.scss </strong>,
-                    the rest of your scss files should be created under this
-                    <strong> ./src/scss/ </strong> directory.
-                </p>
-            </div>
-        </div>
-        <div class="card">
-            <div class="card-body">
-                <h3 class="card-title"> Build it! </h3>
-                <p class="card-text">
-                    By running the command <strong> npm run build </strong>
-                    your app will compile into 3 files under the <strong> dist </strong>
-                    folder, you will find your main html file, your js file
-                    and your css file. .
-                </p>
-                <p class="card-text">
-                    By default your app is going to be build in <strong> Development </strong>
-                    mode, you can switch this to production mode in the webpack config file
-                </p>
-            </div>
-        </div>
-        <div class="card">
-            <div class="card-body">
-                <h3 class="card-title">
-                    Of you go!.
-                </h3>
-                <p class="card-text">
-                    That's all you need to know, have fun, happy codding!. Cheers 🍺🍺.
+                    as the name suggests, this is your home page.
                 </p>
             </div>
         </div>
@@ -90,6 +27,10 @@
 
         &.heading {
             border-top: 5px solid #7fcd91;
+        }
+
+        .card-text {
+            font-size: 1.2em;
         }
     }
 </style>
